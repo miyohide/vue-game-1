a3a0040a6dd0643493807ec40e05fde2af9ffc0b
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-08 20:41:22 +0900

    implement Square component.

---
+++ src/components/Square.vue
@@ -0,0 +1,34 @@
+<!-- ゲームのひとつの枠。O/X/ (空）/の値がvalueとして入る -->
+<template>
+    <button class="square" :class="[ value, { 'winner': winner } ]"
+            :disabled="disabled" @click="click">
+        {{value}}
+    </button>
+</template>
+
+<script>
+  export default {
+    name: 'Square',
+    props: {
+      value: String,
+      winner: Boolean,
+      disabled: Boolean
+    },
+    methods: {
+      click () {
+        this.$emit('click')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .square {
+    }
+    .square.winner {
+    }
+    .square.X {
+    }
+    .square.O {
+    }
+</style>
