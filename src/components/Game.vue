8d8b775c44a95f0233c7b8ce9212177637b832e5
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-15 19:05:27 +0900

    define restart method.

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,10 +1,55 @@
 <template>
+    <div class="game">
+        <div class="game-area">
+            <div class="game-title">
+                <h1>Sample Game</h1>
+            </div>
+
+            <board :squares="squares" />
+
+            <div class="game-info">
+                <p v-if="stepNumber === 0">
+                    ゲームを始めましょう。<b :class="currentPlayer">{{ currentPlayer }}</b>の手番です。
+                </p>
+                <p v-else-if="!!winner">
+                    勝者は
+                    <b :class="currentPlayer">{{ currentPlayer }}</b>
+                    <button>新しいゲームを開始</button>
+                </p>
+                <p v-else-if="stepNumber > 8">
+                    引き分け
+                    <button>新しいゲームを開始</button>
+                </p>
+                <p v-else>
+                    <b :class="currentPlayer">{{ currentPlayer }}</b>
+                </p>
+            </div>
+        </div>
+    </div>
     
 </template>
 
 <script>
+  import Board from './Board'
   export default {
     name: 'Game',
+    components: { Board },
+    data () {
+      return {
+        squares: Array(9).fill(null),
+        stepNumber: 0,
+        currentPlayer: 'X',
+        winner: null
+      }
+    },
+    methods: {
+      restart() {
+        this.squares = Array(9).fill(null)
+        this.stepNumber = 0
+        this.currentPlayer = 'X'
+        this.winner = null
+      }
+    }
   }
 </script>
 
--- tests/unit/game.spec.js
+++ tests/unit/game.spec.js
@@ -2,8 +2,18 @@ import { shallowMount } from '@vue/test-utils'
 import Game from '../../src/components/Game'
 
 describe('Game.vue', () => {
-  it('fire click event', () => {
+  it('when call restart, all data is initialized', () => {
     const wrapper = shallowMount(Game)
-    expect(wrapper.exists()).toBe(true)
+    wrapper.setData({
+      squares: Array(9).fill(10),
+      stepNumber: 5,
+      currentPlayer: 'O',
+      winner: [0, 1, 2]
+    })
+    expect(wrapper.vm.stepNumber).toBe(5)
+    wrapper.vm.restart()
+    expect(wrapper.vm.stepNumber).toBe(0)
+    expect(wrapper.vm.currentPlayer).toBe('X')
+    expect(wrapper.vm.winner).toBeNull()
   })
 })
